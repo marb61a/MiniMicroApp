3e5ffff0bdd342861d8876889f049bd233f00374
Martin <marb61a@gmail.com>
2020-09-15 19:06:26 +0100

    Updated comments index page

--- comments/index.js
+++ comments/index.js
@@ -9,13 +9,23 @@ app.use(bodyParser.json());
 const commentsByPostId = {};
 
 // Setup routes
-app.get('/posts/:id/comments', (req, res) => {
-    const commentId = randomBytes(4).toString('hex'); 
-
+app.get('/posts/:id/comments', (req, res) => {    
+    res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
-    
+    const commentId = randomBytes(4).toString('hex'); 
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({
+        id: commentId,
+        content
+    });
+    commentsByPostId[req.params.id] = comments;
+
+    // Send back entire comments array
+    res.status(201).send(comments);
 });
 
 app.listen(4000, () => {
