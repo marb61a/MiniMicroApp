289ad3ac3858a95e1338051762f41123770dccc2
Martin <marb61a@gmail.com>
2020-09-15 19:13:57 +0100

    Added client src files

---
+++ client/src/PostCreate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+    return <div>
+        <form>
+            <div className="form-group">
+
+            </div>
+        </form>
+    </div>
+};
