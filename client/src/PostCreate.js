fe892808b68cbba47cc80c093d10fa0e7ed7812a
Martin <marb61a@gmail.com>
2020-09-15 19:31:42 +0100

    Updated PostCreate component

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default () => {
-    return <div>
-        <form>
-            <div className="form-group">
+    const [ title, setTitle ] = useState('');
 
-            </div>
-        </form>
-    </div>
+    const onSubmit = async (event) => {
+        event.preventDefault();
+    
+        await axios.post('http://localhost:4000/posts', {
+            title
+        });
+
+        // Empties the input
+        setTitle('');
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input 
+                        value={title}
+                        onChange={e => setTitle(e.target.value)} 
+                        className="form-control" 
+                    />
+                </div>
+                <button className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
 };
