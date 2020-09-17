4f293608320219da914014e2eb0dfe8d1cb19167
Martin <marb61a@gmail.com>
2020-09-17 12:18:33 +0100

    Updated PostList to enable fetching posts

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default () => {
+    const [posts, setPosts] = useState({});
+
+    const fetchPosts = async () => {
+        const res = await axios.get('http://localhost:4000/posts');
+
+        setPosts(res.data);
+    }
+
+    // Only fetches posts when component is first displayed
+    // Empty array as second argument is so that the function
+    // is ran only once
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    // Array of post objects
+    const renderedPosts = Object.values(posts)
+        .map(post => {
+            return (
+                <div 
+                    className="card" 
+                    style={{ width: '30%', marginBottom: '20px' }}
+                    key={post.id}
+                >
+
+                </div>
+            )
+        })
+
     return <div />;
 }
