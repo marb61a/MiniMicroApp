7ab77bd2e089af108fdb90f3a9296e7293ba313e
Martin <marb61a@gmail.com>
2020-09-23 12:01:16 +0100

    Updated moderation index file to allow comment moderation and added to event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,9 +8,14 @@ app.use(bodyParser.json());
 app.post('/events', (req, res) => {
     const event = req.body;
 
+    // Posts
     axios.post('http://localhost:4000/events', event);
+    // Comments
     axios.post('http://localhost:4001/events', event);
+    // Query Service
     axios.post('http://localhost:4002/events', event);
+    // Moderation Service
+    axios.post('http://localhost:4003/events', event);
 
     res.send({ status: 'ok' });
 
